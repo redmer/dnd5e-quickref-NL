e935e6c27c7ecd85985b6f737fcda72a95a10429
Redmer Kronemeijer <kronemeijer@rdmr.eu>
2020-08-16 09:38:53 +0200

    Update data_condition.js

--- js/data_condition.js
+++ js/data_condition.js
@@ -1,14 +1,13 @@
 data_condition = [
     {
-        title: "Verblind",
-        icon: "one-eyed",
-        subtitle: "You can't see",
-        description: "You can't see",
+        title: "Bang",
+        icon: "sharp-smile",
+        subtitle: "You are frightened",
+        description: "You are frightened",
         reference: "PHB, pg. 290.",
         bullets: [
-            "You automatically fail any ability check which requires sight.",
-            "You have disadvantage on attack rolls.",
-            "Attack rolls against you have advantage."
+            "You have disadvantage on ability checks and attack rolls while the source of your fear is within line of sight.",
+            "You can't willingly move closer to the source of your fear."
         ]
     },
     {
@@ -23,6 +22,20 @@ data_condition = [
         ]
     },
     {
+        title: "Bewusteloos",
+        icon: "coma",
+        subtitle: "You are unconscious",
+        description: "You are unconscious",
+        reference: "PHB, pg. 292.",
+        bullets: [
+            "You are incapacitated, can't move or speak, and are unaware of your surroundings.",
+            "You drop whatever you're holding and fall prone.",
+            "Attack rolls against you have advantage.",
+            "Any attack that hits you is a critical hit if the attacker is within 5 feet of you.",
+            "You automatically fail Strength and Dexterity saving throws.",
+        ]
+    },
+    {
         title: "Doofgemaakt",
         icon: "elf-ear",
         subtitle: "You can't hear",
@@ -30,6 +43,63 @@ data_condition = [
         reference: "PHB, pg. 290.",
         bullets: [
             "You automatically fail any ability check which requires hearing."
+        ]
+    },
+    {
+        title: "In bedwang gehouden",
+        icon: "imprisoned",
+        subtitle: "You are restrained",
+        description: "You are restrained",
+        reference: "PHB, pg. 292.",
+        bullets: [
+            "Your speed becomes 0, and you can't benefit from any bonus to your speed.",
+            "You have disadvantage on attack rolls.",
+            "Attack rolls against you have advantage.",
+            "You have disadvantage on Dexterity saving throws."
+        ]
+    },
+    {
+        title: "Liggend",
+        icon: "crawl",
+        subtitle: "You are prone",
+        description: "You are prone",
+        reference: "PHB, pg. 292.",
+        bullets: [
+            "Your only movement option is to crawl, unless you stand up.",
+            "You have disadvantage on attack rolls.",
+            "Attack rolls against you have advantage if the attacker is within 5 feet of you, otherwise the attack roll has disadvantage."
+        ]
+    },
+    {
+        title: "Onzichtbaar",
+        icon: "invisible",
+        subtitle: "Invisible",
+        description: "You can't be seen without the aid of magic or a special sense",
+        reference: "PHB, pg. 291.",
+        bullets: [
+            "For the purpose of hiding, you are heavily obscured.",
+            "You can still be detected by any noise you make or tracks you leave.",
+            "You have advantage on attack rolls.",
+            "Attack rolls against you have disadvantage."
+        ]
+    },
+    {
+        title: "Stervend",
+        icon: "dead-head",
+        subtitle: "You are dying",
+        description: "You have been dropped to zero hit points and are dying",
+        reference: "PHB, pg. 197.",
+        bullets: [
+            "If you are reduced to 0 hit points by damage that fails to kill you, you fall unconscious and are dying.",
+            "If you receive any healing you immediately regain consciousness again and no longer dying.",
+            "When dying, at the start of each of your turns you make a death saving throw. Roll a d20 and do not add any modifiers.",
+            "A 10 or higher is a success, 9 or lower is a failure.",
+            "On your third success, you become stable.",
+            "On your third failure, you die.",
+            "Rolling a 1 counts as two failures.",
+            "Rolling a 20 immediately causes you to regain 1 hit point.",
+            "You can also be stabilized by an ally taking the Stabilize action and succeeding on a DC 10 Wisdom (Medicine) check.",
+            "Once stable, you regain 1 hit point after 1d4 hours."
         ]
     },
     {
@@ -46,14 +116,12 @@ data_condition = [
         ]
     },
     {
-        title: "Bang",
-        icon: "sharp-smile",
-        subtitle: "You are frightened",
-        description: "You are frightened",
+        title: "Uitgeschakeld",
+        icon: "internal-injury",
+        subtitle: "You are Incapacitated",
+        description: "You can't take actions or reactions",
         reference: "PHB, pg. 290.",
         bullets: [
-            "You have disadvantage on ability checks and attack rolls while the source of your fear is within line of sight.",
-            "You can't willingly move closer to the source of your fear."
         ]
     },
     {
@@ -69,25 +137,37 @@ data_condition = [
         ]
     },
     {
-        title: "Uitgeschakeld",
-        icon: "internal-injury",
-        subtitle: "Geen acties of reacties nemen. (Incapacitated)",
-        description: "You can't take actions or reactions",
+        title: "Verblind",
+        icon: "one-eyed",
+        subtitle: "You can't see",
+        description: "You can't see",
         reference: "PHB, pg. 290.",
         bullets: [
+            "You automatically fail any ability check which requires sight.",
+            "You have disadvantage on attack rolls.",
+            "Attack rolls against you have advantage."
         ]
     },
     {
-        title: "Onzichtbaar",
-        icon: "invisible",
-        subtitle: "Invisible",
-        description: "You can't be seen without the aid of magic or a special sense",
-        reference: "PHB, pg. 291.",
+        title: "Verdoofd",
+        icon: "internal-injury",
+        subtitle: "You are stunned",
+        description: "You are stunned",
+        reference: "PHB, pg. 292.",
         bullets: [
-            "For the purpose of hiding, you are heavily obscured.",
-            "You can still be detected by any noise you make or tracks you leave.",
-            "You have advantage on attack rolls.",
-            "Attack rolls against you have disadvantage."
+            "You are incapacitated, can't move, and can speak only falteringly.",
+            "Attack rolls against you have advantage.",
+            "You automatically fail Strength and Dexterity saving throws."
+        ]
+    },
+    {
+        title: "Vergiftigd",
+        icon: "deathcab",
+        subtitle: "You are poisoned",
+        description: "You are poisoned",
+        reference: "PHB, pg. 292.",
+        bullets: [
+            "You have disadvantage on attack rolls and ability checks."
         ]
     },
     {
@@ -115,86 +195,6 @@ data_condition = [
             "You automatically fail Strength and Dexterity saving throws.",
             "You have resistance to all damage.",
             "You are immune to poison and disease, though a poison or disease already in your system is only suspended, not neutralized."
-        ]
-    },
-    {
-        title: "Vergiftigd",
-        icon: "deathcab",
-        subtitle: "You are poisoned",
-        description: "You are poisoned",
-        reference: "PHB, pg. 292.",
-        bullets: [
-            "You have disadvantage on attack rolls and ability checks."
-        ]
-    },
-    {
-        title: "Liggend",
-        icon: "crawl",
-        subtitle: "You are prone",
-        description: "You are prone",
-        reference: "PHB, pg. 292.",
-        bullets: [
-            "Your only movement option is to crawl, unless you stand up.",
-            "You have disadvantage on attack rolls.",
-            "Attack rolls against you have advantage if the attacker is within 5 feet of you, otherwise the attack roll has disadvantage."
-        ]
-    },
-    {
-        title: "In bedwang gehouden",
-        icon: "imprisoned",
-        subtitle: "You are restrained",
-        description: "You are restrained",
-        reference: "PHB, pg. 292.",
-        bullets: [
-            "Your speed becomes 0, and you can't benefit from any bonus to your speed.",
-            "You have disadvantage on attack rolls.",
-            "Attack rolls against you have advantage.",
-            "You have disadvantage on Dexterity saving throws."
-        ]
-    },
-    {
-        title: "Verdoofd",
-        icon: "internal-injury",
-        subtitle: "You are stunned",
-        description: "You are stunned",
-        reference: "PHB, pg. 292.",
-        bullets: [
-            "You are incapacitated, can't move, and can speak only falteringly.",
-            "Attack rolls against you have advantage.",
-            "You automatically fail Strength and Dexterity saving throws."
-        ]
-    },
-    {
-        title: "Bewusteloos",
-        icon: "coma",
-        subtitle: "You are unconscious",
-        description: "You are unconscious",
-        reference: "PHB, pg. 292.",
-        bullets: [
-            "You are incapacitated, can't move or speak, and are unaware of your surroundings.",
-            "You drop whatever you're holding and fall prone.",
-            "Attack rolls against you have advantage.",
-            "Any attack that hits you is a critical hit if the attacker is within 5 feet of you.",
-            "You automatically fail Strength and Dexterity saving throws.",
-        ]
-    },
-    {
-        title: "Stervend",
-        icon: "dead-head",
-        subtitle: "You are dying",
-        description: "You have been dropped to zero hit points and are dying",
-        reference: "PHB, pg. 197.",
-        bullets: [
-            "If you are reduced to 0 hit points by damage that fails to kill you, you fall unconscious and are dying.",
-            "If you receive any healing you immediately regain consciousness again and no longer dying.",
-            "When dying, at the start of each of your turns you make a death saving throw. Roll a d20 and do not add any modifiers.",
-            "A 10 or higher is a success, 9 or lower is a failure.",
-            "On your third success, you become stable.",
-            "On your third failure, you die.",
-            "Rolling a 1 counts as two failures.",
-            "Rolling a 20 immediately causes you to regain 1 hit point.",
-            "You can also be stabilized by an ally taking the Stabilize action and succeeding on a DC 10 Wisdom (Medicine) check.",
-            "Once stable, you regain 1 hit point after 1d4 hours."
         ]
     }
 ]
